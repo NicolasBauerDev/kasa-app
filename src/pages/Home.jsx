a9aec78c65c4ff9bcf48cac09786ba44b81839a2
NicolasBauerV <bauer.nicolas266@gmail.com>
2026-06-08 17:32:24 +0200

    update on overlay hero

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,7 @@
 import { getAllLogements } from "../utils/getLogements";
 import { Link } from "react-router";
+import HeroContainer from "../components/HeroContainer";
+import heroImage from "../assets/home-hero-background.webp";
 import "../styles/pages/home.scss";
 
 export default function Home() {
@@ -7,18 +9,20 @@ export default function Home() {
 
     return (
         <div className="home">
-            <div className="overlay-hero">
+            <HeroContainer imageSource={heroImage} alt="Paysage de montagne">
                 <h1>Chez vous, partout et ailleurs</h1>
-            </div>
+            </HeroContainer>
 
             <div className="grid-logement">
                 {logements.map((logement) => (
                     <Link key={logement.id} to={`/logement/${logement.id}`}>
                         <div className="logement-card">
-                            <img className="logement-cover" src={logement.cover} alt={logement.title} />
-                            <h2 className="logement-title">
-                                {logement.title}
-                            </h2>
+                            <img
+                                className="logement-cover"
+                                src={logement.cover}
+                                alt={logement.title}
+                            />
+                            <h2 className="logement-title">{logement.title}</h2>
                         </div>
                     </Link>
                 ))}
